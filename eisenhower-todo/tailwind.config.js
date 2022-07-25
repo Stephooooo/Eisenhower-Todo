6173535d8ea6928b2111287260fe9a58ec606864
Stephooooo <stephaniembaabu@gmail.com>
2022-07-25 23:11:45 +0300

    updates tailwind.config file

---
+++ eisenhower-todo/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'todo-green': '#36AA36',
+      'todo-orange': '#FFA500',
+      'todo-red': '#FF0000',
+      'todo-blue': '#0000FF',
+      'todo-white': '#FFFFFF',
+      'todo-black': '#000000',
+      'todo-grey':'#D9D9D9',
+    
+    },
+    extend: {},
+  },
+  plugins: [],
+}
